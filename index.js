f707248d1396441a0a3a550210537a567c9af20e
ahm-rubayed <rubayedahmed04@gmail.com>
2022-11-26 11:56:33 +0600

    Payment api added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 const port = process.env.PORT || 5000;
 
@@ -16,7 +17,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try{
+        const usersCollection = client.db('carZone').collection('users')
         const categoriesCollection = client.db('carZone').collection('categories')
+        const bookingCollection = client.db('carZone').collection('booking')
+        const productsCollection = client.db('carZone').collection('products')
+        const paymentsCollection = client.db('carZone').collection('payments');
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.put('/users/seller/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'seller'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
 
         app.get('/categories', async(req, res) => {
             const query = {}
@@ -24,11 +55,81 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/cateogry/:id', async (req, res) => {
+        app.get('/category/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const category = await bookingsCollection.findOne(query);
+            const category = await categoriesCollection.findOne(query);
             res.send(category);
+        })
+
+        app.get('/bookings', async (req, res) => {
+            const query = {};
+            const booking = await bookingCollection.find(query).toArray();
+            res.send(booking);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const product = req.body;
+            const result = await bookingCollection.insertOne(product);
+            res.send(result);
+        });
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+
+        app.post('/products', async (req, res) => {
+            const doctor = req.body;
+            const result = await productsCollection.insertOne(doctor);
+            res.send(result);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const product = req.body;
+            const price = product.price;
+            const amount = price * 10;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        app.post('/payments', async (req, res) =>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.productId
+            const filter = {_id: ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await productsCollection.updateOne(filter, updatedDoc)
+            res.send(result);
         })
     }
     finally{
@@ -36,8 +137,6 @@ async function run() {
     }
 }
 run().catch(err => console.error(err))
-
-
 
 app.get('/', async (req, res) => {
     res.send('carZone server is running');
